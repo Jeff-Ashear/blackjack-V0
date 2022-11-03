f5e27693eeb158d5a665623d5fe87ff2e070c0a1
Jeff.A <kinesthesiac@gmail.com>
2022-11-03 15:26:59 -0700

    card dealing problem solving

---
+++ assets/js/testLogic.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+var deckOfCards = require('./deck.json')
+
+let deck1 = deckOfCards.deck
+
+    $('#startBtn').click(function() {
+        console.log('clicked')
+    })
+
+
+
+
