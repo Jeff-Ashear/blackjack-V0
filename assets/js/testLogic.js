918f5b8b68b0c3bfd2703f33f21dcd52d30cd30d
Jeff Ashear <kinesthesiac@gmail.com>
2022-11-04 00:42:50 -0700

    experimenting with data structures for the card deck

--- assets/js/testLogic.js
+++ assets/js/testLogic.js
@@ -1,10 +1,21 @@
-const fs = require('fs')
-var deckOfCards = require('./deck.json')
+var deckOfCards = {}
+fetch('./assets/js/deck.json').then(response => deckOfCards = response.json())
 
-let deck1 = deckOfCards.deck
+fetch("./assets/js/deck.json")
+    .then(response => {
+        return response.json
+    })
+    .then(deck => console.log(deckOfCards))
+
+// let deck = deckOfCards[2][0]
+
+
+
+
+
 
     $('#startBtn').click(function() {
-        console.log('clicked')
+        console.log(deckOfCards)
     })
 
 
